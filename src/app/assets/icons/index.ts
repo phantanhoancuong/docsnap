20bcb0053fdcc86e9e00f03a6259d6af93881681
Phan Tân Hoàn Cương <132530129+phantanhoancuong@users.noreply.github.com>
2026-05-10 17:01:28 +0700

    feat(assets): add inspection overlay icons
    Changes:
    - Add `ArrowLeftIcon` and `ArrowRightIcon` for image navigation.
    - Add `CropIcon`, `RotateLeftIcon`, and `RetakeIcon` for image manipulation.
    - Add `CompareIcon` to let the user compares the scanned image with the original one.

--- src/app/assets/icons/index.ts
+++ src/app/assets/icons/index.ts
@@ -1,8 +1,14 @@
 export { default as AlbumIcon } from "./photo_library_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+export { default as ArrowLeftIcon } from "./arrow_left_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg";
+export { default as ArrowRightIcon } from "./arrow_right_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg";
 export { default as CameraIcon } from "./photo_camera_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as CloseIcon } from "./close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+export { default as CompareIcon } from "./compare_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
+export { default as CropIcon } from "./crop_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as DownloadIcon } from "./download_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as FlashOffIcon } from "./flash_off_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as FlashOnIcon } from "./flash_on_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as HourglassIcon } from "./hourglass_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+export { default as RetakePhotoIcon } from "./flip_camera_ios_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg";
+export { default as RotateLeftIcon } from "./rotate_left_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
 export { default as ScanIcon } from "./scan_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
